4d6d3abb057b449c93ed80473fdda5b0c6054dfd
Gevorg Minasian <cool.port-965@yandex.ru>
2023-12-27 02:10:18 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,146 @@
+# class Car:
+#     def __init__(self):
+#         self. model = ""
+#         self.year = 0
+#         self.manufacturer = ""
+#         self.engine_capacity = 0.0
+#         self.color = ""
+#         self.price = 0.0
+#
+#     def enterData(self):
+#         self.model = input("Введите названия модели автомобиля: ")
+#         self.year = int(input("Введите год выпуска автомобиля: "))
+#         self.manufacturer = input("Введите производителя автомобиля: ")
+#         self.engine_capacity = float(input("Введите обьем двигателя автомобиля: "))
+#         self.color = input("Введите цвет автомобиля: ")
+#         self.price = float(input("Введите цену автомобиля: "))
+#
+#     def displayData(self):
+#         print("Названия модели: ", self.model)
+#         print("Год выпуска: ", self.year)
+#         print("Производитель: ", self.manufacturer)
+#         print("Объем двигателя ", self.engine_capacity)
+#         print("Цвет машины: ", self.color)
+#         print("Цена: ", self.price)
+#
+#     def getModel(self):
+#         return self.model
+#
+#     def getYear(self):
+#         return self.year
+#
+#     def getManufacturer(self):
+#         return self.manufacturer
+#
+#     def getEngineCapacity(self):
+#         return self.engine_capacity
+#
+#     def getColor(self):
+#         return self.color
+#
+#     def getPrice(self):
+#         return self.price
+#
+# auto = Car()
+# auto.enterData()
+# print()
+# auto.displayData()
+# print()
+#
+# print("Название модели: ", auto.getModel())
+# print("Год выпуска: ", auto.getYear())
+# print("Производитель: ", auto.getManufacturer())
+# print("Объем двигателя: ", auto.getEngineCapacity())
+# print("Цвет машины: ", auto.getColor())
+# print("Цена: ", auto.getPrice())
+#
+
+# 2 Задание
+
+# class Book():
+#     Name_of_book = ""
+#     Year = 0
+#     Publisher = ""
+#     Genre = ""
+#     Author = ""
+#     Price = 0.0
+#
+#     def enter_data(self):
+#         self.Name_of_book = input("Введите названия книги: ")
+#         self.Year = int(input("Введите год выпуска книги: "))
+#         self.Publisher = input("Введите издателя книги: ")
+#         self.Genre = input("Введите жанр книги: ")
+#         self.Author = input("Введите автора книги: ")
+#         self.Price = float(input("Введите цену книги: "))
+#
+#
+#     def display_data(self):
+#         print("Названия книги: ", self.Name_of_book)
+#         print("Год выпуска книги: ", self.Year)
+#         print("Издатель книги: ", self.Publisher)
+#         print("Жанр книги: ", self.Genre )
+#         print("Автор книги: ", self.Author)
+#         print("Цена книги: ", self.Price)
+#
+    # def get_Name_of_book(self):
+    #     return self.Name_of_book
+    #
+    # def get_Year(self):
+    #     return self.Year
+    #
+    # def get_Publisher(self):
+    #     return self.Publisher
+    #
+    # def get_Genre(self):
+    #     return self.Genre
+    #
+    # def get_Author(self):
+    #     return self.Author
+    #
+    # def get_price(self):
+    #     return self.Price
+#
+# Book1 = Book()
+# Book1.enter_data()
+# Book1.display_data()
+
+# class Stadium():
+#     name_of_stadium = ""
+#     openening_date = ""
+#     country = ""
+#     city = ""
+#     Capacity = 0
+#
+#     def enter_date(self):
+#         self.name_of_stadium = input("Введите название стадиона: ")
+#         self.openening_date = input("Введите дату открытия: ")
+#         self.country = input("Введите название страны: ")
+#         self.city = input("Введите название города: ")
+#         self.Capacity = int(input("Введите вместимость стадиона: "))
+#
+#     def display_data(self):
+#         print("Название стадиона: ", self.name_of_stadium)
+#         print("Дата открытия: ", self.openening_date)
+#         print("Название страны: ", self.country)
+#         print("Название города: ", self.city)
+#         print("Вместимость стадиона: ", self.Capacity)
+#
+#     def get_Name_of_stadium(self):
+#         return self.name_of_stadium
+#
+#     def get_date(self):
+#         return self.openening_date
+#
+#     def get_country(self):
+#         return self.country
+#
+#     def get_city(self):
+#         return self.city
+#
+#     def get_capacity(self):
+#         return self.Capacity
+#
+# Stadium1 = Stadium()
+# Stadium1.enter_date()
+# Stadium1.display_data()
+
